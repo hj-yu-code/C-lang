3290db024f63e86ab4036be2514e146ec2d0d4c9
Hyehwa-hj <yu9219@naver.com>
2022-03-11 18:59:04 +0900

    2022.03.11

--- 13_etc/score_pointer.c
+++ 13_etc/score_pointer.c
@@ -3,14 +3,15 @@
 
 void scoreCalc(int(*tmp)[7], int row, int col);
 //배열포인터 변수 (앞의 행은 생략해도 됨. 그 의미는 앞의 행수는 상관없다는 소리 but!!! 열은 절대 생략 안됨!!!)
+void heading();
 
 int main(){
-	int score[5][4] = { {78, 90, 61, 65},
+	int score[5][7] = { {78, 90, 61, 65},
                         {70, 10, 61, 65},
                         {78, 30, 51, 65},
                         {78, 70, 71, 75},
                         {28, 10, 91, 55}
-        };
+                        };
 
 	int row, col, i, j;
 	
@@ -24,13 +25,12 @@ int main(){
 		for (j = 0; j < col; j++)
 			printf("%3d, dndn", score[i][j]);
 		printf("\n");
-
 	}
 
 	return 0;
 }
 
-heading(){
+void heading(){
 	printf("============================================\n");
 	printf("국어  영어 수학 과학 총점 평균 석차 \n");
 	printf("============================================\n");
@@ -40,7 +40,7 @@ heading(){
 // score 배열의 합, 평균, 석차를 계산
 //scoreCalc(int (*tmp)[4]) //tmp라는 이름으로 바꾸고 싶음.-> 배열포인터 변수
 //아래랑 같은 말.
-void scoreCalc(int (*tmp) [7], int row, int col) //배열포인터 변수 (앞의 행은 생략해도 됨. 그 의미는 앞의 행수는 상관없다는 소리 but!!! 열은 절대 생략 안됨!!!)
+void scoreCalc(int(*tmp)[7], int row, int col) //배열포인터 변수 (앞의 행은 생략해도 됨. 그 의미는 앞의 행수는 상관없다는 소리 but!!! 열은 절대 생략 안됨!!!)
 {
 	int i, j;
 	
